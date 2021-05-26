6c104a734f5b6bd4b532a5047c21436899955154
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-25 20:09:28 -0400

    First commit

---
+++ exercises_for_programmers/count_chars.go
@@ -0,0 +1,29 @@
+/*
+Create a program that prompts for an input string and dis- plays output that shows the input string and the number of characters the string contains.
+Example Output
+What is the input string? Homer Homer has 5 characters.
+*/
+
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Enter a word: ")
+	var word string
+	fmt.Scanln(&word)
+
+	count_chars(word) 
+}
+
+func count_chars(word) int{
+	
+	total_chars = 0
+
+	for i := 0; i < len(word); i ++ {
+		total_chars += 1
+	}
+	return total_chars
+
+}
