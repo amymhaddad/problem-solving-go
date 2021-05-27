a7badfa065bdb6d75d2e84b4bda7dc03d80fccff
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-27 08:04:29 -0400

    Updating pizza exercise to perform the return string in the main function

---
+++ exercises_for_programmers/v2_pizza.go
@@ -0,0 +1,43 @@
+/*
+Write a program to evenly divide pizzas. Prompt for the number of people, the number of pizzas, and the number of slices per pizza. Ensure that the number of pieces comes out even. Display the number of pieces of pizza each person should get. If there are leftovers, show the number of leftover pieces.
+Example Output
+       How many people? 8
+       How many pizzas do you have? 2
+	   8 people with 2 pizzas
+Each person gets 2 pieces of pizza. There are 0 leftover pieces.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(calculate_pizza_slices())
+}
+
+func calculate_pizza_slices() (string, string, string){
+
+	var people, pizzas, slices_per_pizza int
+
+	fmt.Printf("How many people? \n")
+	fmt.Scanf("%d/n", &people)
+
+	fmt.Printf("How many pizzas do you have? \n")
+	fmt.Scanf("%d/n", &pizzas)
+
+
+	fmt.Printf("How many slices per pizza? \n")
+	fmt.Scanf("%d/n", &slices_per_pizza)
+	
+	total_pizza_slices := pizzas * slices_per_pizza 
+	slices_per_person := total_pizza_slices / people
+	leftover_slices := total_pizza_slices % people
+
+
+	return strconv.Itoa(total_pizza_slices), strconv.Itoa(slices_per_person), strconv.Itoa(leftover_slices)
+
+
+}
