fdc1e844c1f0321582afc8a490998587cdeaffd8
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-27 11:05:34 -0400

    Completed passwords problem

--- exercises_for_programmers/passwords.go
+++ exercises_for_programmers/passwords.go
@@ -1,4 +1,5 @@
 /*
+Exercise 15
 Create a simple program that validates user login credentials. The program must prompt the user for a username and password. The program should compare the password given by the user to a known password. If the password matches, the program should display “Welcome!” If it doesn’t match, the program should display “I don’t know you.”
 */
 
@@ -6,24 +7,24 @@ package main
 
 import "fmt"
 
+const known_password = "123abc"
+
 func main() {
 	fmt.Println(passwords())
 }
 
-
-//Define constants in or outside of the function? 
-//Need to add username question once i learn how to ask for multiple inputs
 func passwords() string {
-	const known_password = "123abc"
+	var username, password string
 
-	fmt.Println("Enter your password: ")
-	var entered_password string
-	fmt.Scanln(&entered_password)
+	fmt.Printf("Enter your username: ")
+	fmt.Scanf("%s/n", &username)
 
-	if known_password == entered_password {
+	fmt.Printf("Enter your password: ")
+	fmt.Scanf("%s/n", &password)
+
+	if known_password == password {
 		return "Welcome"
 	} else {
 		return "I don't know you"
 	}
-
 }
--- exercises_for_programmers/v2_pizza.go
+++ exercises_for_programmers/v2_pizza.go
@@ -1,4 +1,5 @@
 /*
+Exercise 8
 Write a program to evenly divide pizzas. Prompt for the number of people, the number of pizzas, and the number of slices per pizza. Ensure that the number of pieces comes out even. Display the number of pieces of pizza each person should get. If there are leftovers, show the number of leftover pieces.
 Example Output
        How many people? 8
