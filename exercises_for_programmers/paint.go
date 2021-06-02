c6a09157cf1d9990450dbb7a164e58b0f3198640
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-02 07:16:41 -0400

    Solved it

--- exercises_for_programmers/paint.go
+++ exercises_for_programmers/paint.go
@@ -8,13 +8,6 @@ import (
 
 const paint_gallon_coverage = 350
 
-func main() {
-	room_square_footage := room_size()
-	paint_cans := total_paint_cans(room_square_footage)
-	
-	fmt.Printf("You'll need to purchase %s cans of paint to cover %s square feet", strconv.Itoa(room_square_footage), paint_cans)
-}
-
 func room_size() int {
 	var length, width int 
 	
@@ -30,4 +23,10 @@ func total_paint_cans(room_square_footage int) string{
 	paint_gallons := float64(room_square_footage) / float64(paint_gallon_coverage)
 	total_paint_cans := math.Ceil(paint_gallons)
 	return strconv.FormatFloat(total_paint_cans, 'f', 0, 64)
+}
+
+func main() {
+	room_square_footage := room_size()
+	paint_cans := total_paint_cans(room_square_footage)
+	fmt.Printf("You'll need to purchase %s cans of paint to cover %s square feet.\n", paint_cans, strconv.Itoa(room_square_footage))
 }
