e1751986c4f47b5012f5d49cb2222b2a9e55c585
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-02 07:54:56 -0400

    Added functions

--- exercises_for_programmers/bmi.go
+++ exercises_for_programmers/bmi.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 )
 
+const bmi_low_boundary float64 = 18.5
+const bmi_high_boundary float64 = 25
+
 func main() {
 	height, weight := user_height_weight()
-	fmt.Println(height, weight)
+//	fmt.Println(height, weight)
+
+	bmi := calculate_bmi(height, weight)
+//	fmt.Println(bmi)
+
+	is_normal_weight := bmi_range(bmi)
+	fmt.Println(is_normal_weight)
 }
 
 func user_height_weight() (float64, float64) {
@@ -23,3 +32,12 @@ func user_height_weight() (float64, float64) {
 	return user_height, user_weight
 
 }
+
+func calculate_bmi(height float64, weight(float64)) (float64) {
+	return (weight / (height * height)) * 703
+}
+
+func bmi_range(bmi float64) (bool) {
+	return bmi >= bmi_low_boundary && bmi <= bmi_high_boundary
+}
+
