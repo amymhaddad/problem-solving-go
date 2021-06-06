db2bc7dfe163646202feec5eee13c3d6031b0e2f
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-06 07:52:06 -0400

    Ready for PR

--- exercises_for_programmers/bad_input.go
+++ exercises_for_programmers/bad_input.go
@@ -6,12 +6,6 @@ import (
 	"strconv"
 )
 
-func main() {
-	rate := user_rate()
-	fmt.Println(validate_user_rate(rate))
-
-}
-
 func user_rate() string {
 
 	var rate string
@@ -23,21 +17,24 @@ func user_rate() string {
 func validate_user_rate(rate string) int {
 	for {
 		valid_rate, _ := regexp.MatchString("[1-9]+", rate)
-		fmt.Println("valid", valid_rate)
 		if !valid_rate{
 			fmt.Printf("Sorry. '%s' is an invalid rate. Try again.\n", rate)
 			rate = user_rate()
 			fmt.Println(rate)
 			continue
 		} else {
-			user_rate, _ := strconv.Atoi(rate)
-			return calculate_investment_return(user_rate)
+			user_investment_rate, _ := strconv.Atoi(rate)
+			return calculate_investment_return(user_investment_rate)
 		}
 	}
 }
 
 func calculate_investment_return(user_rate int) int {
 	return 72 / user_rate
-
 }	
 
+func main() {
+	rate_of_return := user_rate()
+	years_to_double_investment := validate_user_rate(rate_of_return)
+	fmt.Printf("It'll take %d years to double your investment.\n", years_to_double_investment) 
+}
