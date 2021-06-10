111f226f9a5ab1c1fb07e9af9be10e3efda1c99e
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-10 07:55:09 -0400

    First commit

---
+++ exercises_for_programmers/filter.go
@@ -0,0 +1,28 @@
+/*
+Create a program that prompts for a list of numbers, sepa- rated by spaces. Have the program print out a new list con- taining only the even numbers.
+*/
+
+package main
+
+import "fmt"
+
+func user_numbers() {
+	var number_list []int	
+
+	for {
+		var number int
+		fmt.Println("Enter a number: 	
+
+
+	}
+
+}
+
+
+
+
+
+func main() {
+	number_list := user_numbers()
+	fmt.Println(number_list)
+}
