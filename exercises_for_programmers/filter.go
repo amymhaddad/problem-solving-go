7319d8a544fef009d03ac3e159c49f2d50ec4a14
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-11 08:02:05 -0400

    User input function works:

--- exercises_for_programmers/filter.go
+++ exercises_for_programmers/filter.go
@@ -4,25 +4,64 @@ Create a program that prompts for a list of numbers, sepa- rated by spaces. Have
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func user_numbers() {
-	var number_list []int	
-
-	for {
-		var number int
-		fmt.Println("Enter a number: 	
-
-
+func user_input() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter a list a numbers, separated by a space: ")
+	scanner.Scan()
+	if scanner.Err() != nil {
+		fmt.Println("Error: ", scanner.Err())
 	}
-
+	user_numbers := scanner.Text()
+	return user_numbers
 }
 
+// func quote_and_author() (string, string) {
+// 	scanner := bufio.NewScanner(os.Stdin)
+// 	fmt.Print("Enter your quote: ")
+  
+// 	scanner.Scan()
+  
+// 	if scanner.Err() != nil {
+// 		fmt.Println("Error: ", scanner.Err())
+// 	}
+  
+// 	quote := scanner.Text()
+  
+// 	var author string
+// 	fmt.Printf("Enter the author of the quote: ")
+// 	fmt.Scanf("%s", &author)
+// 	return quote, author
+//  }
+ 
 
+func generate_number_range(user_numbers string)  {
+	user_nums := strings.Split(user_numbers, " ")
+	even_nums := [] int{}
 
+	for _, num := range user_nums {
+		digit, _ := strconv.Atoi(num)
+		if digit % 2 == 0 {
+			even_nums = append(even_nums, digit)
+		}
+	}
+	fmt.Println(even_nums)
+}
 
 
 func main() {
-	number_list := user_numbers()
-	fmt.Println(number_list)
+	fmt.Println(user_input())
+
+	// user_numbers := user_input()
+	// fmt.Println(user_numbers)
+	// number_range := generate_number_range(user_numbers)
+	// fmt.Println(user_numbers)
 }
+
