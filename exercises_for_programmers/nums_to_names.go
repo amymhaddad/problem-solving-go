1a9d59805408466f6ac946008861efc608e46d25
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-12 08:03:23 -0400

    First commit

---
+++ exercises_for_programmers/nums_to_names.go
@@ -0,0 +1,22 @@
+/*
+Write a program that converts a number from 1 to 12 to the corresponding month. Prompt for a number and display the corresponding calendar month, with 1 being January and 12 being December. For any value outside that range, display an appropriate error message.
+*/
+
+package main
+
+import "fmt"
+	
+
+func user_number() {
+	var number int
+
+	fmt.Println("Enter a number: ")
+	fmt.Scanf("%d", number)
+
+//	if number 
+}
+
+
+func main() {
+	fmt.Println("vim-go")
+}
