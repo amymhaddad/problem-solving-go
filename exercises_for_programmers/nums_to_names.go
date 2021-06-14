0e732eaf681ac4040026bd0407a8dfa4f60837eb
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-14 08:02:54 -0400

    Solved it

--- exercises_for_programmers/nums_to_names.go
+++ exercises_for_programmers/nums_to_names.go
@@ -4,19 +4,36 @@ Write a program that converts a number from 1 to 12 to the corresponding month. 
 
 package main
 
-import "fmt"
-	
+import (
+	"fmt"
+)
 
-func user_number() {
+var months = map[int]string {1: "January", 2: "February", 3: "March", 4: 
+"April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September", 10: 
+"October", 11: "November", 12: "December"}
+
+func user_number() (int, error) {
 	var number int
+	start_month := 1 
+	end_month := 12
 
 	fmt.Println("Enter a number: ")
-	fmt.Scanf("%d", number)
+	fmt.Scanf("%d", &number)
 
-//	if number 
+	if number < start_month || number > end_month {
+		return number, fmt.Errorf("Invalid number")
+	}
+	return number, nil
 }
-
 
 func main() {
-	fmt.Println("vim-go")
-}
+	for {
+		calendar_num, err := user_number()
+		if err == nil {
+			fmt.Printf("The number of the month is %s", months[calendar_num])
+			break 
+		} else {
+			fmt.Println("Please try again.")
+		}	
+	}
+ }
